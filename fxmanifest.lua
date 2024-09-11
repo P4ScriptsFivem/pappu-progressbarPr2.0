de437b145d8fb1c115f1c16e52de444b35960da4
Pappu100 <120780563+Pappu100code@users.noreply.github.com>
2024-09-11 17:52:54 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+author 'pappu'
+description 'pappu-progressbar creating progressbars in QB-Core.'
+version '1.0.0'
+
+
+ui_page 'nui/index.html'
+
+client_script 'client.lua'
+
+files {
+    'nui/index.html',
+    'nui/style.css',
+    'nui/script.js'
+}
